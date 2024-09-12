721e167bceafa8f317c21c87359e83224f701200
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-09-12 17:23:42 +0000

    added regex to extract txns

--- src/st_dashboard.py
+++ src/st_dashboard.py
@@ -1,10 +1,14 @@
 import os
+import re
+import string
 import time
 from tempfile import TemporaryDirectory
 
+import pandas as pd
 import psycopg2
 import pytesseract
 import streamlit as st
+from dateutil import parser as date_parser
 from dotenv import load_dotenv
 from pdf2image import convert_from_path
 from sqlalchemy import MetaData, create_engine, select
@@ -36,6 +40,21 @@ with TemporaryDirectory() as tempdir:
         with open(f"page_{i}.txt", mode="w") as f:
             f.write(text)
 
+regex = r"^(?P<card_no>\d{4})\s+(?P<txn_date>\d{2}/\d{2})\s+(?P<post_date>\d{2}/\d{2})\s(?P<ref_no>[a-zA-Z0-9]+)\s*(?P<desc>.*?)(?P<txn_amt>\d+\.\d{2})"
+punctuation_remover = str.maketrans("", "", string.punctuation)
+
+df = pd.DataFrame(columns=["card_no", "txn_date", "desc", "amount"])
+for i, text in enumerate(texts):
+    matches = re.finditer(regex, text, re.MULTILINE)
+    for match in matches:
+        card_no = match.group("card_no")
+        txn_date = date_parser.parse(match.group("txn_date")).strftime("%Y-%m-%d")
+        post_date = date_parser.parse(match.group("post_date")).strftime("%Y-%m-%d")
+        desc = match.group("desc").translate(punctuation_remover).strip()
+        txn_amt = match.group("txn_amt").strip().strip("$")
+        df.loc[df.shape[0], :] = (card_no, txn_date, desc, txn_amt)
+
+st.dataframe(df)
 
 metadata = MetaData()
 metadata.reflect(bind=engine)
