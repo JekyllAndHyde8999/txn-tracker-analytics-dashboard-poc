5bcfc852084a3e2b693156b9a34fe3a2d9f28813
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-09-12 16:43:43 +0000

    added pdf upload and OCR

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM python:3.12
 
 WORKDIR /app
 
+RUN apt-get update
+RUN apt-get install poppler-utils -y
+RUN apt-get install tesseract-ocr -y
+
 COPY ./src .
 
 RUN pip install -r requirements.txt
--- src/st_dashboard.py
+++ src/st_dashboard.py
@@ -1,29 +1,47 @@
 import os
 import time
+from tempfile import TemporaryDirectory
 
 import psycopg2
+import pytesseract
 import streamlit as st
 from dotenv import load_dotenv
+from pdf2image import convert_from_path
 from sqlalchemy import MetaData, create_engine, select
 
 load_dotenv()
 
-st.title("ST+PostgreSQL+Docker Integration Test")
+st.title("Transaction Tracker")
 
 username = os.getenv("POSTGRES_USERNAME")
 password = os.getenv("POSTGRES_PASSWORD")
 host = os.getenv("POSTGRES_HOST")
 database_name = os.getenv("POSTGRES_DBNAME")
+
 connection_url = f"postgresql+psycopg2://{username}:{password}@{host}/{database_name}"
 engine = create_engine(connection_url)
 
+tx_file = st.file_uploader("Upload PDF file", type="pdf")
+if not tx_file:
+    st.stop()
+
+with TemporaryDirectory() as tempdir:
+    with open(os.path.join(tempdir, tx_file.name), mode="wb") as f:
+        f.write(tx_file.getvalue())
+
+    images = convert_from_path(os.path.join(tempdir, tx_file.name))
+    texts = [pytesseract.image_to_string(image=image) for image in images]
+
+    for i, text in enumerate(texts, 1):
+        with open(f"page_{i}.txt", mode="w") as f:
+            f.write(text)
+
+
 metadata = MetaData()
 metadata.reflect(bind=engine)
-st.write(metadata.tables)
 member = metadata.tables["member"]
 query = select(member.c.name, member.c.birthday).order_by(member.c.birthday)
 st.write(query)
 
 with engine.connect() as connect:
     result = list(connect.execute(query))
-    st.write(result[0])
